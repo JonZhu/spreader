4dbda0ca7622b42489bc5ad4772a4e098791855b
zhujun <zhujun@ZHUJUN-PC>
2015-02-09 17:59:22 +0800

    http deliver增加 var和sleep定义

--- src/main/java/com/juncke/spreader/deliver/impl/HttpDeliver.java
+++ src/main/java/com/juncke/spreader/deliver/impl/HttpDeliver.java
@@ -5,9 +5,11 @@ package com.juncke.spreader.deliver.impl;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
@@ -47,47 +49,26 @@ public class HttpDeliver extends AbstractDeliver implements IDeliver {
 			return IDeliver.RESULT_SUCCESS;
 		}
 		
-		List<Element> requestEles = config.elements("request");
-		if (requestEles.isEmpty()) {
+		Iterator<Element> configChildIter = config.elementIterator();
+		if (!configChildIter.hasNext()) {
 			return IDeliver.RESULT_SUCCESS;
 		}
 		
 		CloseableHttpClient client = HttpClients.createDefault();
 		try {
-			for (Element requestEle : requestEles) {
-				String method = requestEle.attributeValue("method", "get");
-				HttpRequestBase request = null;
-				if ("POST".equalsIgnoreCase(method)) {
-					HttpPost httpPost = new HttpPost();
-					
-					// post param
-					List<Element> paramEles = requestEle.selectNodes("params/param");
-					if (paramEles != null && !paramEles.isEmpty()) {
-						List<NameValuePair> paramList = new ArrayList<NameValuePair>();
-						for (Element paramEle : paramEles) {
-							paramList.add(new BasicNameValuePair(paramEle.attributeValue("name"), replcaceContent(paramEle.getText(), content)));
-						}
-						httpPost.setEntity(new UrlEncodedFormEntity(paramList));
-					}
-					
-					request = httpPost;
+			while (configChildIter.hasNext()) {
+				Element childEle = configChildIter.next();
+				
+				String eleName = childEle.getName();
+				if ("var".equals(eleName)) {
+					executeVar(childEle);
+				} else if ("request".equals(eleName)) {
+					executeRequest(client, childEle, content);
+				} else if ("sleep".equals(eleName)) {
+					executeSleep(childEle);
 				} else {
-					request = new HttpGet();
+					LOG.warn("http deliver不支持 {} 操作", eleName);
 				}
-				
-				request.setURI(new URI(requestEle.attributeValue("url")));
-				
-				// header
-				List<Element> headerEles = requestEle.selectNodes("headers/header");
-				if (headerEles != null && !headerEles.isEmpty()) {
-					for (Element headerEle : headerEles) {
-						request.addHeader(headerEle.attributeValue("name"), headerEle.getText());
-					}
-				}
-				
-				// cookie todo
-				
-				client.execute(request);
 			}
 		} catch (Exception e) {
 			LOG.error("Deliver执行出错", e);
@@ -97,6 +78,84 @@ public class HttpDeliver extends AbstractDeliver implements IDeliver {
 		}
 		
 		return IDeliver.RESULT_SUCCESS;
+	}
+
+
+	/**
+	 * 执行Sleep
+	 * 
+	 * @param sleepEle
+	 * @author zhujun
+	 * @date 2015-2-9
+	 */
+	private void executeSleep(Element sleepEle) {
+		String sleepMs = sleepEle.getText();
+		if (StringUtils.isNumeric(sleepMs)) {
+			LOG.debug("Start sleep: {} ms", sleepMs);
+			try {
+				Thread.sleep(Long.valueOf(sleepMs));
+			} catch (Exception e) {
+				LOG.error("sleep出错", e);
+			}
+		}
+	}
+
+
+	/**
+	 * 执行http请求
+	 * 
+	 * @param childEle
+	 * @author zhujun
+	 * @date 2015-2-9
+	 */
+	private void executeRequest(CloseableHttpClient client, Element childEle, String content) throws Exception {
+		String method = childEle.attributeValue("method", "get");
+		HttpRequestBase request = null;
+		if ("POST".equalsIgnoreCase(method)) {
+			HttpPost httpPost = new HttpPost();
+			
+			// post param
+			List<Element> paramEles = childEle.selectNodes("params/param");
+			if (paramEles != null && !paramEles.isEmpty()) {
+				List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+				for (Element paramEle : paramEles) {
+					paramList.add(new BasicNameValuePair(paramEle.attributeValue("name"), replcaceContent(paramEle.getText(), content)));
+				}
+				httpPost.setEntity(new UrlEncodedFormEntity(paramList));
+			}
+			
+			request = httpPost;
+		} else {
+			request = new HttpGet();
+		}
+		
+		request.setURI(new URI(childEle.attributeValue("url")));
+		
+		// header
+		List<Element> headerEles = childEle.selectNodes("headers/header");
+		if (headerEles != null && !headerEles.isEmpty()) {
+			for (Element headerEle : headerEles) {
+				request.addHeader(headerEle.attributeValue("name"), headerEle.getText());
+			}
+		}
+		
+		// cookie todo
+		
+		client.execute(request);
+		
+	}
+
+
+	/**
+	 * 执行变量初始化
+	 * 
+	 * @param varEle
+	 * @author zhujun
+	 * @date 2015-2-9
+	 */
+	private void executeVar(Element varEle) {
+		// TODO Auto-generated method stub
+		
 	}
 
 

a377494c44cefbc04d83013e2d9392c4396fb411
zhujun <juncke@163.com>
2015-02-12 23:00:40 +0800

    测试htmlunit

--- src/main/java/com/juncke/spreader/deliver/impl/WebBroserDeliver.java
+++ src/main/java/com/juncke/spreader/deliver/impl/WebBroserDeliver.java
@@ -3,8 +3,16 @@
  */
 package com.juncke.spreader.deliver.impl;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+
 
 /**
  * 模拟
@@ -15,13 +23,57 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
  */
 public class WebBroserDeliver extends AbstractDeliver {
 
+	private final static Logger LOG = LoggerFactory.getLogger(WebBroserDeliver.class);
+	
+	
 	@Override
 	public int delive(String content) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.firefox.bin", "E:/Program Files/Mozilla Firefox/firefox.exe");
 		
-		WebDriver webDriver = new HtmlUnitDriver(true);
-		webDriver.get("http://www.baidu.com");
+		String id = "id32f45tt6248";
+		String mail = id + "@qq.com";
+		String passwd = "www123mmm";
+		
+		final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_24);
+		try {
+			// 注册
+			HtmlPage page = webClient.getPage("https://passport.58.com/reg/");
+			
+			HtmlForm regForm = page.getFormByName("submitForm");
+			page.getAnchorByText("邮箱注册").click();
+			
+			// regForm.setActionAttribute("/doregister");
+			
+			regForm.getInputByName("nickName").setValueAttribute(id);
+			regForm.getInputByName("txtemail").setValueAttribute(mail);
+			regForm.getInputByName("password").setValueAttribute(passwd);
+			regForm.getInputByName("cpassword").setValueAttribute(passwd);
+			
+			HtmlPage page2 = ((HtmlSubmitInput)regForm.getInputByValue("立即注册")).click();
+			
+			System.out.println(page2.asText());
+			
+			
+			// 发布信息
+			HtmlPage publishPage = webClient.getPage("http://post.58.com/102/23087/s5");
+			HtmlForm pForm = publishPage.getFormByName("aspnetForm");
+			pForm.getInputByName("Title").setValueAttribute("驾校照人3000");
+			pForm.getInputByValue("656331").click();
+			pForm.getInputByValue("617390").click();
+			pForm.getInputByValue("617405").click();
+			((HtmlElement)publishPage.getFirstByXPath("//div[@class='editor']")).setTextContent("驾校照人3000,驾校照人3000,驾校照人3000,驾校照人3000,驾校照人3000,");
+			pForm.getTextAreaByName("Content").setText("驾校照人3000,驾校照人3000,驾校照人3000,驾校照人3000,驾校照人3000,");
+			pForm.getInputByName("goblianxiren").setValueAttribute("杨先生");
+			pForm.getInputByName("Phone").setValueAttribute("13859873215");
+			pForm.getSelectByName("localArea").setDefaultValue("11487");
+			pForm.getSelectByName("localDiduan").setDefaultValue("11984");
+			pForm.getInputByName("dizhi").setValueAttribute("驾校驾校驾校");
+			
+			HtmlPage page3 = ((HtmlSubmitInput)publishPage.getElementById("fabu")).click();
+			System.out.println(page3.asText());
+		} catch (Exception e) {
+			LOG.error("error", e);
+		}
 		
 		return 0;
 	}
